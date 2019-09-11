7fa8e0872ebb1f9842853b5d62932b13cb3b4efc
Richard Persson <richard0persson@gmail.com>
2019-09-11 17:31:47 +0200

    DateTime finished

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -1,11 +1,21 @@
 <?php
 
-class DateTimeView {
+// DateTimeView class. Gets the current date and time.
+class DateTimeView
+{
+	// Returns the current date and time.
+	public function show()
+	{
+		date_default_timezone_set('Europe/Stockholm');
+
+		$dayName = date('l');
+		$dayNumber = date('jS');
+		$month = date('F');
+		$year = date('Y');
+		$time = date('H:i:s');
 
 
-	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = "$dayName, the $dayNumber of $month $year, The time is $time";
 
 		return '<p>' . $timeString . '</p>';
 	}
